cf22d089b05bf21b3c1f84720fc10bd107d982c1
abdul96523 <113436354+abdul96523@users.noreply.github.com>
2023-02-18 22:26:20 +0530

    Commit 2
    Minor changes

--- src/components/PlayersInput.tsx
+++ src/components/PlayersInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import '../assets/styles/matchSettings.css'
 type propsType = {
   teamPlayers: string[];
