cea708d162d0a03b14390f40b70f2acfc4ce4393
abdul96523 <113436354+abdul96523@users.noreply.github.com>
2023-02-19 18:16:29 +0530

    commit 2
    Added icons and proper styling

--- src/components/MatchSettings.tsx
+++ src/components/MatchSettings.tsx
@@ -1,19 +1,31 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/styles/matchSettings.css";
 import PlayersInput from "./PlayersInput";
-
+import { GiTennisBall,GiCricketBat } from "react-icons/gi"
+import {BiCoin} from "react-icons/bi"
+import {AiFillSetting} from "react-icons/ai"
 const MatchSettings = () => {
-  const [firstTeamTitle, changeFirstTeamTitle] = useState("");
-  const [secondTeamTitle, changeSecondTeamTitle] = useState("");
-  const [displaySelectionPanel, changeDisplaySelectionPanel] = useState(false);
-  const [proceedButtonDisable, changeProceedDisable] = useState(true);
-  const [startButtonDisable, changeStartDisable] = useState(true);
+  const [firstTeamTitle, changeFirstTeamTitle] = useState<string>("");
+  const [secondTeamTitle, changeSecondTeamTitle] = useState<string>("");
+  const [newTeam, setNewTeam] = useState<string>("");
+  const [tossWinner, setTossWinner] = useState("");
+  const [optedOption, setOptedOption] = useState("");
+
+  const [displaySelectionPanel, changeDisplaySelectionPanel] = useState<boolean>(false);
+  const [proceedButtonDisable, changeProceedDisable] = useState<boolean>(true);
+  const [startButtonDisable, changeStartDisable] = useState<boolean>(true);
+  const [addTeamPannel, hideAddTeamPannel] = useState<boolean>(true);
+  const [addButtonDisabled, changeDisabled] = useState(true);
+
   const [teamList] = useState<string[]>([
     "Chennai super kings",
     "Mumbai Indians",
     "Royal Challengers",
     "rajisthan Royals",
     "Sunrisers hyderbad",
+    "Gujrat titans",
+    "lucknow kings",
+    "delhi capitals"
   ]);
   const [TeamOnePlayers, changeTeamOnePlayers] = useState<string[]>([
     "jack",
@@ -47,21 +59,20 @@ const MatchSettings = () => {
   const [selectedTeamTwo, handleSelectedTeamTwo] = useState<string[]>([]);
 
   useEffect(() => {
-    if(firstTeamTitle===secondTeamTitle)
-    {
-      changeProceedDisable(true)
-      return
+    if (firstTeamTitle === secondTeamTitle||optedOption==='') {
+      changeProceedDisable(true);
+      return;
     }
-    if (firstTeamTitle !== "" && secondTeamTitle !== "")
+    if (firstTeamTitle !== "" && secondTeamTitle !== ""&&optedOption!=='')
       changeProceedDisable(false);
-  }, [firstTeamTitle, secondTeamTitle]);
+  }, [firstTeamTitle, secondTeamTitle,optedOption]);
 
   useEffect(() => {
     if (
       !selectedTeamOne.includes("") &&
       !selectedTeamTwo.includes("") &&
-      selectedTeamOne.length != 0 &&
-      selectedTeamTwo.length != 0
+      selectedTeamOne.length !== 0 &&
+      selectedTeamTwo.length !== 0
     )
       changeStartDisable(false);
   }, [selectedTeamOne, selectedTeamTwo]);
@@ -77,6 +88,7 @@ const MatchSettings = () => {
   };
 
   const handleBackButton = () => {
+    changeStartDisable(true);
     changeDisplaySelectionPanel(false);
   };
   const teamTwoPlaying11 = (list: string[]) => {
@@ -85,74 +97,182 @@ const MatchSettings = () => {
   const teamOnePlaying11 = (list: string[]) => {
     handleSelectedTeamTwo(list);
   };
+  const handleAddTeam = () => {
+    hideAddTeamPannel(false);
+  };
+  const handleAddButton = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    hideAddTeamPannel(true);
+    changeDisplaySelectionPanel(false);
+    //after adding new team to backend need to set newTeam=''
+    //setNewTeam('')
+  };
+  const handleTeamNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTeam(e.target.value);
+  };
+
+  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setTossWinner(e.target.value);
+  };
 
   return (
     <>
-      {!displaySelectionPanel && (
-        <div className="container">
-          <div className="d-flex align-items-center justify-content-center mx-4 my-1">
-            <p className="fs-2 text-capitalize text-white fw-bolder">
-              match setup
-            </p>
+      {!addTeamPannel && (
+        <div className="container mt-4 ">
+          <div className="row justify-content-center mt-3 p-4">
+            <div className="col-7 addTeamPanel border border-dark rounded">
+              <form onSubmit={handleAddButton}>
+                <div className="mb-3 p-4">
+                  <h3>Team Name</h3>
+                  <input
+                    type="text"
+                    className="form-control "
+                    placeholder="Enter Team Name Here!!"
+                    value={newTeam}
+                    onChange={handleTeamNameInput}
+                  />
+                </div>
+                <div className="d-flex align-items-center justify-content-center pb-3">
+                  <button
+                    type="submit"
+                    className="btn btn-success fs-3 fw-bolder"
+                    disabled={addButtonDisabled}
+                  >
+                    Add Team
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+      {!displaySelectionPanel && addTeamPannel && (
+        <div className="container mt-5">
+          <div className="d-flex align-items-center justify-content-end">
+            <button
+              className="btn btn-primary  border border-light fs-3  fw-bold"
+              onClick={handleAddTeam}
+            >
+              Add Team +
+            </button>
+          </div>
+          <div className=" mt-4">
+            <h1 className=" text-capitalize text-white fw-bolder">
+              match setup<AiFillSetting className="ps-1 ms-1 "></AiFillSetting>
+            </h1>
           </div>
           <form onSubmit={handleProceed}>
-            <div className="row d-flex justify-content-around">
+            <div className="row d-flex justify-content-around mt-3 mb-3 p-3">
               <div className="col-5 bg-light border border-dark rounded p-4">
-                <h3 className="d-flex align-items-center justify-content-center">
-                  Team 1
-                </h3>
+                <h3>Team 1</h3>
                 <select
                   className="form-select text-capitalize fs-3"
                   value={firstTeamTitle}
                   onChange={(e) => {
                     changeFirstTeamTitle(e.target.value);
+                    setTossWinner("")
                     // invoke changeTeamOneplayers from here
-                    console.log('team players changed');
+                    //console.log("team players changed");
                   }}
-                  placeholder="Enerasdas"
                 >
-                  <option value="" disabled >
+                  <option value="" disabled>
                     Select Team 1
                   </option>
-                  {teamList.map((team) => (
-                    <option value={team} key={team}>
-                      {team}
-                    </option>
-                  ))}
+                  {teamList.map((team) => {
+                    if(team!==secondTeamTitle){
+                      return ( <option value={team} key={team}>
+                        {team}
+                      </option>)
+                    }
+                  })}
                 </select>
               </div>
               <div className="col-2 d-flex justify-content-center align-items-center fs-1 fw-bolder text-light">
                 VS
               </div>
               <div className="col-5 bg-light border border-dark rounded p-4">
-                <h3 className="d-flex align-items-center justify-content-center">
-                  Team 2
-                </h3>
+                <h3>Team 2</h3>
                 <select
-                  className="form-select text-capitalize fs-3"
+                  className="form-select text-capitalize fs-3 "
                   value={secondTeamTitle}
                   onChange={(e) => {
                     changeSecondTeamTitle(e.target.value);
+                    setTossWinner("")
                     // invoke changeTeamOneplayers from here
-                    console.log('team players changed');
+                    //console.log("team players changed");
                   }}
                 >
-                  <option value="" disabled >
+                  <option value="" disabled>
                     Select Team 2
                   </option>
-                  {teamList.map((team) => (
-                    <option value={team} key={team}>
-                      {team}
-                    </option>
-                  ))}
+                  {teamList.map((team) => {
+                    if(team!==firstTeamTitle){
+                      return ( <option value={team} key={team}>
+                        {team}
+                      </option>)
+                    }
+                  })}
                 </select>
               </div>
             </div>
-            <div className="d-flex align-items-center justify-content-center m-3">
+            <div className="bg-light rounded">
+              {tossWinner.length === 0 && <div className="d-inline-flex text-capitalize p-4">
+                <h4 className=" pe-2 fw-bold ps-2">Toss <BiCoin className=" me-2 text-warning"></BiCoin> </h4>
+                <div className=" d-inline-flex ms-3 me-5">
+                  <input
+                    type="radio"
+                    value={firstTeamTitle}
+                    id="male"
+                    onChange={handleChange}
+                    name="tossWinner"
+                  />
+                  <h4 className="ms-1">{(firstTeamTitle.length === 0) ? 'Team 1' : firstTeamTitle}</h4>
+                </div>
+                <div className=" d-inline-flex">
+                  <input
+                    type="radio"
+                    value={secondTeamTitle}
+                    id="female"
+                    onChange={handleChange}
+                    name="tossWinner"
+                  />
+                  <h4 className="ms-1">{(secondTeamTitle.length === 0) ? 'Team 2' : secondTeamTitle}</h4>
+                </div>
+              </div>}
+              {tossWinner.length !== 0 && <div className="p-4 text-capitalize pb-3">
+                <div className="d-inline-flex text-capitalize ">
+                  <p className=" pe-2 fs-4 fw-bold mt-1 mb-0 ms-2">{tossWinner} Opted to</p>
+                  <div className=" d-inline-flex ms-3 me-5">
+                    <input
+                      type="radio"
+                      value='Batting'
+                      id="male"
+                      onChange={(e => setOptedOption(e.target.value))}
+                      name="optedOption"
+                    />
+                    <h4 className="ms-1"><GiCricketBat className="text-warning fs-1"></GiCricketBat></h4>
+                  </div>
+                  <div className=" d-inline-flex">
+                    <input
+                      type="radio"
+                      value='Bowling'
+                      id="female"
+                      onChange={(e => setOptedOption(e.target.value))}
+                      name="optedOption"
+                    />
+                    <h4 className="ms-1"><GiTennisBall className="text-success fs-1"></GiTennisBall></h4>
+                  </div>
+                </div>
+                <div className="float-end ">
+                  <button type="button" className="btn btn-danger fw-bold border border-light fs-5 " onClick={()=>{setTossWinner('');setOptedOption('')}}>Change Toss Winner</button>
+                </div>
+              </div>}
+            </div>
+            <div className="d-flex align-items-center justify-content-center m-4">
               <button
                 disabled={proceedButtonDisable}
                 type="submit"
-                className="btn btn-dark proceedButton border border-light fs-4  fw-bold"
+                className="btn btn-dark proceedButton border border-light fs-2  fw-bold"
               >
                 Proceed
               </button>
@@ -162,26 +282,26 @@ const MatchSettings = () => {
       )}
       {displaySelectionPanel && (
         <div className="container">
-          <div className="d-flex align-items-center justify-content-between">
-            <p className="fs-2 text-capitalize text-white fw-bolder">
+          <div className="row justify-content-around mt-2 mb-2">
+            <p className="fs-3 text-capitalize text-white fw-bolder col-6 me-5 p-0">
               Team Players selection
             </p>
             <button
               type="submit"
-              className="btn btn-dark backButton fs-4  fw-bold border border-light border-1"
+              className="btn btn-dark backButton fs-5 fw-bold border border-light border-2 col-6 ms-5 mt-2 "
               onClick={handleBackButton}
             >
               Back
             </button>
           </div>
           <form onSubmit={handleStartMatch}>
-            <div className="row d-flex justify-content-around">
+            <div className="row justify-content-around ">
               <div
                 className="col-4  border border-dark rounded ps-4 pe-4 pt-1 pb-1"
                 style={{ backgroundColor: "white" }}
               >
-                <h3 className="d-flex align-items-center justify-content-center text-capitalize">
-                  {firstTeamTitle}
+                <h3 className="d-flex fw-bold align-items-center justify-content-center text-capitalize mt-1">
+                  {firstTeamTitle}{(1>2)?<GiCricketBat className="text-warning fs-2"></GiCricketBat>:<GiTennisBall className="text-success fs-2 ms-2"></GiTennisBall>}
                 </h3>
                 <hr className="mt-0"></hr>
                 <PlayersInput
@@ -193,7 +313,7 @@ const MatchSettings = () => {
                 className="col-4  border border-dark rounded ps-4 pe-4 pt-1 pb-1"
                 style={{ backgroundColor: "white" }}
               >
-                <h3 className="d-flex align-items-center justify-content-center text-capitalize">
+                <h3 className="d-flex fw-bold align-items-center justify-content-center text-capitalize mt-1">
                   {secondTeamTitle}
                 </h3>
                 <hr className="mt-0"></hr>
@@ -207,19 +327,18 @@ const MatchSettings = () => {
             <div className="d-flex align-items-center justify-content-center m-3">
               <button
                 type="submit"
-                className="btn btn-dark border border-light border-1 fs-4  fw-bold startMatch "
+                className="btn btn-dark border border-light border-2 fs-4  fw-bold startMatch "
                 disabled={startButtonDisable}
               >
                 Start Match
               </button>
             </div>
           </form>
-          <p className="text-light fs-4">
-            Note: Select all the players of both teams to enable Start Match .
-          </p>
         </div>
       )}
     </>
+
+
   );
 };
 
--- src/components/PlayersInput.tsx
+++ src/components/PlayersInput.tsx
@@ -1,10 +1,10 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import '../assets/styles/matchSettings.css'
 type propsType = {
   teamPlayers: string[];
-  playing11:(arg:string[])=>void
+  playing11: (arg: string[]) => void
 };
-const PlayersInput = ({ teamPlayers,playing11 }: propsType) => {
+const PlayersInput = ({ teamPlayers, playing11 }: propsType) => {
   const [player1, setPlayer1] = useState<string>("");
   const [player2, setPlayer2] = useState<string>("");
   const [player3, setPlayer3] = useState<string>("");
@@ -29,269 +29,287 @@ const PlayersInput = ({ teamPlayers,playing11 }: propsType) => {
     player10,
     player11
   ];
-  
-  useEffect(()=>{playing11(list)},[player1,player2,player3,player4,player5,player6,player7,player8,player9,player10,player11])
+
+  useEffect(() => { playing11(list) }, [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11])
+
+  const handleOption = (x: string) => {
+    if (x === '') return <option value='' disabled>Select Player</option>
+    else return <option>Change Player</option>
+  }
   const optionList = teamPlayers.filter((player) => !list.includes(player));
+
   return (
     < div className="d-flex justify-content-center flex-column">
-      <div className="d-inline-flex ">
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 1"
           value={player1}
           onChange={(e) => setPlayer1(e.target.value)}
-          className="playerInput text-capitalize "
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player1}
             onChange={(e) => {
               setPlayer1(e.target.value);
             }}
           >
-            <option >Select Player </option>
+
+            {handleOption(player1)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 " >
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-2">
         <input
           placeholder="Player 2"
           value={player2}
           onChange={(e) => setPlayer2(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player2}
             onChange={(e) => {
               setPlayer2(e.target.value);
             }}
           >
-            <option>Select Player </option>
+            {handleOption(player2)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 3"
           value={player3}
           onChange={(e) => setPlayer3(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player3}
             onChange={(e) => {
               setPlayer3(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player3)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 4"
           value={player4}
           onChange={(e) => setPlayer4(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player4}
             onChange={(e) => {
               setPlayer4(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player4)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 5"
           value={player5}
           onChange={(e) => setPlayer5(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player5}
             onChange={(e) => {
               setPlayer5(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player5)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 6"
           value={player6}
           onChange={(e) => setPlayer6(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player6}
             onChange={(e) => {
               setPlayer6(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player6)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 7"
           value={player7}
           onChange={(e) => setPlayer7(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player7}
             onChange={(e) => {
               setPlayer7(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player7)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 8"
           value={player8}
           onChange={(e) => setPlayer8(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player8}
             onChange={(e) => {
               setPlayer8(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player8)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 9"
           value={player9}
           onChange={(e) => setPlayer9(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player9}
             onChange={(e) => {
               setPlayer9(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player9)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 10"
           value={player10}
           onChange={(e) => setPlayer10(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player10}
             onChange={(e) => {
               setPlayer10(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player10)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
           </select>
         }
       </div>
-      <div>
+      <div className="d-inline-flex mb-3">
         <input
           placeholder="Player 11"
           value={player11}
           onChange={(e) => setPlayer11(e.target.value)}
-          className="playerInput text-capitalize"
+          className="playerInput text-capitalize me-2 ps-3 rounded-start "
+          readOnly
         />
         {
           <select
-            className="dropdownList  mb-3 text-capitalize"
+            className="dropdownList text-capitalize ps-2 rounded-end"
             value={player11}
             onChange={(e) => {
               setPlayer11(e.target.value);
             }}
           >
-            <option>Select Player</option>
+            {handleOption(player11)}
             {optionList.map((player) => (
-              <option key={player} value={player}>
+              <option key={player} value={player} className="p-4 fs-5 ">
                 {player}
               </option>
             ))}
