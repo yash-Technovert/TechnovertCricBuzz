a61d7bff69f04b6843154d3562f233d7fe73a53a
abdul96523 <113436354+abdul96523@users.noreply.github.com>
2023-02-22 16:46:49 +0530

    responsiveness

--- src/components/MatchStatus.tsx
+++ src/components/MatchStatus.tsx
@@ -9,7 +9,7 @@ type propType={
 
  const MatchStatus = ({teamName,teamOneDetails,teamTwoDetails,index,matchWinner}:propType) => {
   return (
-    <Card  >
+    <Card  className='border border-dark'>
       <Card.Header className='d-flex'><p className=' fw-bold fs-4 m-auto'>Match {index+1}</p></Card.Header>
       <Card.Body className='text-capitalize'>
         <Card.Title className='fw-bold text-nowrap'>{teamName[0]}</Card.Title><p className=' fs-lg-4 fs-sm-6 '>{teamOneDetails[0]}/{teamOneDetails[1]} ({teamOneDetails[2]}.0 Overs )</p>
--- src/components/MatchView.tsx
+++ src/components/MatchView.tsx
@@ -22,7 +22,7 @@ const MatchView = () => {
   },
   {
     teamOne:"Technoverts kings",
-    teamTwo:"Team legends",
+    teamTwo:"The Technovert Falcons",
     tossWinner:"Technoverts kings",
     tossDecision:"Bowl",
     teamOneRuns:89,
@@ -167,32 +167,37 @@ const MatchView = () => {
       matchTime: "12:00:00",
     },
   ]);
-//🔴
+  const teamTitle=(title:string)=>{
+    var words=title.split(' ')
+    var res:string='';
+    words.forEach(x=>res+=x.charAt(0).toUpperCase())
+    return res;
+  }
   return (
     <div className="container mt-4 ">
       <div className="row bg-light m-4 rounded p-2 border border-dark">
-        <p className="fs-3 fw-bold  mb-0">LIVE <CiStreamOn className="fs-3 text-danger"></CiStreamOn></p>
-        <div className="row  pb-3 d-flex">
+        <p className="fs-3 fw-bold " style={{textAlign:'center'}}>LIVE <CiStreamOn className="fs-3 text-danger"></CiStreamOn></p>
+        <div className="row  pb-3 d-flex m-auto">
           {liveMatches.length=== 0 && <p className='col-4 fs-3 m-auto font-dark'>No current live matches.</p>}
           {liveMatches.length !== 0 &&
             liveMatches.map((match, index) => {
               return (
                 <div className="col-lg-6">
-                  <Card className="border " >
-                    <Card.Header className="d-flex">
-                    <Card.Title className="fw-bold text-nowrap fs-4">
-                        {match.teamOne} VS {match.teamTwo}
+                  <Card className="border border-dark" >
+                    <Card.Header >
+                        <Card.Title className="fw-bold d-flex " >
+                        <p className="m-auto fw-bold">{teamTitle(match.teamOne)} VS  {teamTitle(match.teamTwo)}</p>
                       </Card.Title>
                     </Card.Header>
                     <Card.Body className="text-capitalize">
-                      <Card.Text className="text-capitalize  fs-5">
-                        {match.tossWinner} won the toss and elected to {match.tossDecision} .
+                      <Card.Text className="text-capitalize " style={{fontSize:'1rem'}}>
+                        {match.tossWinner} won the toss and elected to {match.tossDecision} 
                       </Card.Text>
-                      <Card.Title className="d-flex ">
-                        <p className="text-primary">{match.teamOne}</p> {(match.teamOneRuns===0)?<p className="ms-auto">(Yet to Bat)</p>:<p className="ms-auto">{match.teamOneRuns}/{match.teamOneWickets} ({match.teamOneOverPlayed} Overs)</p>} 
+                      <Card.Title className="d-md-flex ">
+                        <p className="text-primary text-nowrap fw-bold">{match.teamOne}</p> {(match.teamOneRuns===0)?<p className="ms-auto">(Yet to Bat)</p>:<p className="ms-auto text-nowrap ">{match.teamOneRuns}/{match.teamOneWickets} ({match.teamOneOverPlayed} Overs)</p>} 
                       </Card.Title>
-                      <Card.Title className="d-flex ">
-                        <p className="text-primary">{match.teamTwo}</p> {(match.teamTwoRuns===0)?<p className="ms-auto">(Yet to Bat)</p>:<p className="ms-auto">{match.teamTwoRuns}/{match.teamTwoWickets} ({match.teamTwoOverPlayed} Overs)</p>}
+                      <Card.Title className="d-md-flex ">
+                        <p className="text-primary text-nowrap fw-bold" style={{}}>{match.teamTwo}</p> {(match.teamTwoRuns===0)?<p className="ms-auto">(Yet to Bat)</p>:<p className="ms-auto text-nowrap">{match.teamTwoRuns}/{match.teamTwoWickets} ({match.teamTwoOverPlayed} Overs)</p>}
                       </Card.Title>
                       <hr></hr>
                       <div className="d-flex ">
@@ -211,8 +216,8 @@ const MatchView = () => {
         </div>
       </div>
       <div className="row bg-light m-4 rounded p-2">
-        <p className=" fs-3 fw-bold mb-0">COMPLETED </p>
-        <div className="row row-cols-xl-3 row-cols-md-2 row-cols-sm-1 pb-3">
+        <p className=" fs-3 fw-bold mb-0" style={{textAlign:'center'}}>COMPLETED </p>
+        <div className="row row-cols-xl-3 row-cols-md-2 row-cols-sm-1 pb-3 d-flex m-auto">
           {finishedMatches.map((matchDetails, index) => {
             return (
               <div className="col">
